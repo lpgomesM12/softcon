06c0e896ce8e407fce4a3882dc2ebcaddff8befe
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-07-25 16:29:54 -0300

    alteração secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :deploy_to, '/home/deploy/softcon'
 # set :keep_releases, 5
 
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
