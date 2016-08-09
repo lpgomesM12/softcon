b0b131df8b48670da0f421960bb1b02309f9ceda
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-08-09 16:28:47 -0300

    alteração productin.eb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -77,10 +77,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.precompile += %w('.woff', '.eot', '.svg', '.ttf')
-
-  config.assets.paths << Rails.root.join("app","assets","fonts","audio")
 
 
  config.action_mailer.delivery_method = :smtp
